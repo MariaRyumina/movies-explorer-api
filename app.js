74a4b252c1d0b225c42df509f1c853514e19d794
Maria Ryumina <maria.ryumin@gmail.com>
2023-11-17 23:04:58 +0300

    feat: добавила основную функциональность

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const {
   PORT = 3000,
-  MONGO_URL = 'mongodb://127.0.0.1:27017/moviesdb',
+  MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
 } = process.env;
 
 const helmet = require('helmet');
